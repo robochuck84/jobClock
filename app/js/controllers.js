732f7406bea4fc4aeee2ca67f450074d102ca75e
Travis Rogers <trogers@gmail.com>
2014-06-24 21:48:05 +0100

    moved timer controllers out and added angular-timer.ls

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,44 @@
 'use strict';
 
 /* Controllers */
+angular.module('jobClock.controllers', []).
+    controller('TimerPanelController', function($scope) {
+        $scope.timers = [
+            {
+                id: 1,
+                title: "Maintenance",
+                start: "12:00pm",
+                end: "",
+                isRunning: true,
+                group: 1
+            },
+            {
+                id: 2,
+                title: "Publish Everything",
+                start: "12:00pm",
+                end: "",
+                isRunning: false,
+                group: 2,
+            },
+            {
+                id: 3,
+                title: "Ops",
+                start: "12:00pm",
+                end: "",
+                isRunning: false,
+                group: 3
+            }
+        ];
+        $scope.timerClicked = function(id) {
+            _($scope.timers).each(function(element, index, list) {
+                if (element.id === id) {
+                    element.isRunning = true;
+                } else {
+                    element.isRunning = false;
+                }
+            });
+        };
+    });
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
 
-  }]);
